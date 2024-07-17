adfb95b5add0ae9989b4984a10ef4590496250e4
Ajit <zlksnkwork@gmail.com>
2024-07-17 23:17:13 +0530

    Update hey-init-nixos.sh

--- scripts/hey-init-nixos.sh
+++ scripts/hey-init-nixos.sh
@@ -10,8 +10,9 @@ nix-channel --add https://github.com/nix-community/home-manager/archive/release-
 nix-channel --update
 
 # Setup devenv
-nix-env -iA cachix -f https://cachix.org/api/v1/install
-cachix use devenv
+#nix-env -iA cachix -f https://cachix.org/api/v1/install
+# --- i install cachix directly on nixos now so maybe the above cmd is not needed anymore (while the below ones still do)
+#cachix use devenv
 # May fail if DNS is not setup (won't be able to find githubusercontent in JioFiber).
 # If that's the case then reboot and re-run this script.
-nix-env -if https://install.devenv.sh/latest
+#nix-env -if https://install.devenv.sh/latest
